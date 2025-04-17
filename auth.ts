191852a0a760e5d44b3e0652679e548e2175efa9
Cassius <cassius.reynolds.dev@gmail.com>
2025-04-17 09:19:01 -0700

    init supabase cli, added next_auth to supabase/config.toml schemas array, added supabase adapter and sb env vars

--- auth.ts
+++ auth.ts
@@ -1,8 +1,13 @@
+import { SupabaseAdapter } from "@auth/supabase-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  adapter: SupabaseAdapter({
+    url: process.env.SUPABASE_URL || '',
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY || '',
+  }),
   callbacks: {
     async redirect({ baseUrl }) {
       return baseUrl;
