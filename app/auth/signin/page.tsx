ea6923b8cf895b448fa6d6117676a227e58543fb
Cassius <cassius.reynolds.dev@gmail.com>
2025-04-16 22:40:14 -0700

    added callback url to bring user back to home page after signing in with google

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,6 @@
+import SignIn from "@/app/_components/SignIn";
+
+const page = () => {
+  return <SignIn />;
+};
+export default page;
--- auth.ts
+++ auth.ts
@@ -1,6 +1,11 @@
-import NextAuth from "next-auth"
-import Google from 'next-auth/providers/google'
- 
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-})
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+  },
+});
