900a1e070c6024458340cefea8a96e6effb92602
Cassius <cassius.reynolds.dev@gmail.com>
2025-04-19 16:53:34 -0700

    adding use-debounce library for seach debouncing

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -1,15 +1,19 @@
 "use server";
-const BASE_VOL_URL = `https://www.googleapis.com/books/v1/volumes?q=`
-
+const BASE_VOL_URL = `https://www.googleapis.com/books/v1/volumes?q=`;
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+import axios from "axios";
 import { signIn, signOut } from "./auth";
 
 export const signInWithGoogle = async () => await signIn("google");
 export const singOutAction = async () => await signOut();
 
-export const searchByTitle = async () => {
-try {
-    
-} catch (err) {
+export const searchByTitleAction = async (query: string) => {
+  try {
+    const response = await axios.get(
+      `${BASE_VOL_URL}${query}&key=${GOOGLE_API_KEY}`
+    );
+    return response.data;
+  } catch (err) {
     console.error(err);
-}
-}
+  }
+};
