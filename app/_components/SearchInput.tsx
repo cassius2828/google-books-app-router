91a30664509e404afcb7a0007542ac0df14b0d4d
Cassius <cassius.reynolds.dev@gmail.com>
2025-04-22 17:31:16 -0700

    added button to clear input value

--- app/_components/SearchInput.tsx
+++ app/_components/SearchInput.tsx
@@ -21,12 +21,21 @@ const SearchInput = () => {
     handleSetSearchQuery();
   }, [value]);
   return (
-    <input
-      type="text"
-      placeholder="Search books..."
-      onChange={(e) => setQuery(e.target.value)}
-      className="w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4"
-    />
+    <div className="relative">
+      <input
+        type="text"
+        placeholder="Search books..."
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+        className="w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4"
+      />
+      <button
+        onClick={() => setQuery("")}
+        className="absolute right-3 top-2 text-2xl text-gray-500/90"
+      >
+        x
+      </button>
+    </div>
   );
 };
 export default SearchInput;
