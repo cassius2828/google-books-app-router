14c74c6fdd365a318f14d6e3b74a29d4b0e22339
Cassius <cassius.reynolds.dev@gmail.com>
2025-04-22 21:24:27 -0700

    removed unused vars and imports

--- app/_components/Marquee3D.tsx
+++ app/_components/Marquee3D.tsx
@@ -1,84 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
-import { cn } from "@/app/_lib/utils";
-import { Marquee } from "./marquee";
-import { getBooksByTitle } from "../_lib/service";
 import { faker } from "@faker-js/faker";
+import { getBooksByTitle } from "../_lib/service";
 import { Book } from "../_lib/types";
 import BookCard from "./Books/BookCard";
-
-const reviews = [
-  {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
-  },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
-  },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
-  },
-];
-
-const ReviewCard = ({
-  img,
-  name,
-  username,
-  body,
-}: {
-  img: string;
-  name: string;
-  username: string;
-  body: string;
-}) => {
-  return (
-    <figure
-      className={cn(
-        "relative h-full w-fit sm:w-36 cursor-pointer overflow-hidden rounded-xl border p-4",
-        // light styles
-        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-        // dark styles
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
-      )}
-    >
-      <div className="flex flex-row items-center gap-2">
-        <img className="rounded-full" width="32" height="32" alt="" src={img} />
-        <div className="flex flex-col">
-          <figcaption className="text-sm font-medium dark:text-white">
-            {name}
-          </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">{username}</p>
-        </div>
-      </div>
-      <blockquote className="mt-2 text-sm">{body}</blockquote>
-    </figure>
-  );
-};
+import { Marquee } from "./marquee";
 
 export async function Marquee3D() {
   const word = faker.word.noun();
   console.log(word, " \n\n<-- generated word \n\n");
   const data = await getBooksByTitle(word);
-  const books: Book[] = data.items.slice(0,20);
+  const books: Book[] = data.items.slice(0, 20);
   console.log(books, " \n\n<-- books \n\n");
   const firstRow = books.slice(0, books.length / 2);
   const secondRow = books.slice(books.length / 2);
