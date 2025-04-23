60ae44db68361068a764580f80a8fbe7b8685588
Cassius <cassius.reynolds.dev@gmail.com>
2025-04-23 11:46:14 -0700

    consolidated functions into reusable fns and got basic query building working. Now must implement other fields and query together to build full str

--- app/_lib/utils.ts
+++ app/_lib/utils.ts
@@ -5,34 +5,19 @@ import { AdvancedSearchParams, SearchParam } from "./types";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const buildAdvancedSearchParamsQuery = (
-  params: AdvancedSearchParams
-) => {
-  let finalStr = "";
-
-  for (const key in params) {
-    const valueObj: SearchParam = params[key as keyof AdvancedSearchParams];
-
-    if (valueObj.value) {
-      if (key === "volumeId") return `/${valueObj.value}`;
-      if (valueObj.type !== "query") finalStr += `&${key}=${valueObj.value}`;
-    }
-  }
-
-  return finalStr;
-};
-
 const buildFullTextQuery = (str: string) => {
+  if (!str) return;
   str = str.split(" ").join("+");
   return str;
 };
 const buildExactPhraseQuery = (str: string) => {
+  if (!str) return;
   str = '"'.concat(str, '"');
   return str;
 };
 
 const buildExcludeTextQuery = (str: string) => {
+  if (!str) return;
   let finalStr = "";
   let tempStr = "";
   const multWords = str.split(" ");
@@ -44,6 +29,7 @@ const buildExcludeTextQuery = (str: string) => {
 };
 
 const buildIncludesTextQuery = (str: string) => {
+  if (!str) return;
   let finalStr = "";
   const multWords = str.split(" ");
   if (multWords.length > 1) {
@@ -53,8 +39,69 @@ const buildIncludesTextQuery = (str: string) => {
   } else return str;
 };
 
+export const buildAdvancedSearchParamsQuery = (
+  params: AdvancedSearchParams
+) => {
+  let finalStr = "";
+  let queryStr = "q=";
 
+  for (const key in params) {
+    const valueObj: SearchParam = params[key as keyof AdvancedSearchParams];
 
+    if (valueObj.value) {
+      if (key === "volumeId") return `/${valueObj.value}`;
+      if (valueObj.type !== "query") finalStr += `&${key}=${valueObj.value}`;
+      else {
+        if (key === "fullText") {
+          queryStr = handleQueryAddition(
+            valueObj.value,
+            queryStr,
+            buildFullTextQuery
+          );
+        }
+        if (key === "exactPhrase") {
+          queryStr = handleQueryAddition(
+            valueObj.value,
+            queryStr,
+            buildExactPhraseQuery
+          );
+        }
+        if (key === "excludeText") {
+          queryStr = handleQueryAddition(
+            valueObj.value,
+            queryStr,
+            buildExcludeTextQuery
+          );
+        }
+        if (key === "includesText") {
+          queryStr = handleQueryAddition(
+            valueObj.value,
+            queryStr,
+            buildIncludesTextQuery
+          );
+        }
+      }
+    }
+  }
+
+  return {finalStr, queryStr};
+};
+const handleQueryAddition = (
+  value: string,
+  queryStr: string,
+  fn: (value: string) => string | void
+) => {
+  const newStr = fn(value);
+  if (newStr) {
+    queryStr += newStr;
+    return queryStr;
+  } else return queryStr;
+};
+
+// let newStr = buildFullTextQuery(valueObj.value);
+// if (newStr) {
+//   queryStr += newStr;
+// }
 const exampleObj: AdvancedSearchParams = {
   // replace spaces with + -- q=example+here+you+go
   fullText: {
@@ -108,7 +155,7 @@ const exampleObj: AdvancedSearchParams = {
   },
   // q=intitle:Series+of+unfortunate+events
   title: {
-    value: "",
+    value: "John Simmons",
     type: "query",
   },
   // q=inpublisher:Tin+House
@@ -125,3 +172,4 @@ const exampleObj: AdvancedSearchParams = {
 console.log(buildAdvancedSearchParamsQuery(exampleObj));
 // ex of a with minus query
 // https://www.google.com/search?tbo=p&tbm=bks&q=lord+-rings&num=10
+
