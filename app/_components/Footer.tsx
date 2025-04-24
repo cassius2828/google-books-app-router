9280d073511ab33d9eb2edc84cf44d0ea679d484
Cassius <cassius.reynolds.dev@gmail.com>
2025-04-24 12:57:11 -0700

    added logo to header and footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,10 +1,14 @@
+import Image from "next/image";
+import logo from "@/app/icon.png";
+
 // components/Footer.tsx
 export default function Footer() {
-    return (
-      <footer className="bg-white border-t mt-12">
-        <div className="container mx-auto px-6 py-4 text-center text-gray-600">
-          © 2025 LibrisList. All rights reserved.
-        </div>
-      </footer>
-    );
-  }
+  return (
+    <footer className="bg-white border-t mt-12">
+      <div className="container mx-auto px-6 py-4 text-center text-gray-600 flex justify-center items-center gap-4">
+        © 2025 LibrisList. All rights reserved.
+        <Image src={logo} alt="LibrisList Logo" width={40} height={40} />
+      </div>{" "}
+    </footer>
+  );
+}
--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,10 +1,11 @@
 // components/Header.tsx
 
+import Image from "next/image";
 import { auth } from "../_lib/auth";
 import { getPublicUserID } from "../_lib/service";
 import DesktopNav from "./Nav/DesktopNav";
 import MobileNav from "./Nav/MobileNav";
-
+import logo from "@/app/icon.png";
 export default async function Header() {
   const session = await auth();
   let publicUserID;
@@ -14,7 +15,15 @@ export default async function Header() {
   return (
     <header className="bg-white shadow-md relative z-50">
       <div className="container mx-auto flex items-center justify-between px-6 py-4">
-        <div className="text-2xl font-semibold text-gray-800">LibrisList</div>
+        <div className="text-2xl font-semibold text-gray-800 flex gap-2 items-center">
+          LibrisList
+          <Image
+          src={logo}
+          alt="LibrisList Logo"
+          width={40}
+          height={40}
+        />
+        </div>
         {/* desktop */}
         <DesktopNav publicUserID={publicUserID} />
         {/* mobile */}
