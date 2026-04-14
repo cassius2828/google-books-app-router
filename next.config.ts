faefc882f299b6f10922f9fd29b0160083203cf8
Cassius Reynolds <cassius.reynolds.dev@gmail.com>
2026-04-14 16:31:32 -0700

    Refactor image configuration in next.config.ts: replace domains with remotePatterns for enhanced image handling and support for multiple protocols and pathnames.

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,33 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "books.google.com",
-      "png.pngtree.com",
-      "d2uth2nw0znbpc.cloudfront.net",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "http",
+        hostname: "books.google.com",
+        pathname: "/books/**",
+      },
+      {
+        protocol: "https",
+        hostname: "books.google.com",
+        pathname: "/books/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "png.pngtree.com",
+      },
+      {
+        protocol: "https",
+        hostname: "d2uth2nw0znbpc.cloudfront.net",
+      },
     ],
   },
 };
